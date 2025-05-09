d91953e7f17badc3edd6eed473043fecc25c9435
Sitthikorn Mangkala <157336868+M-Sitthikorn@users.noreply.github.com>
2025-05-09 13:38:06 +0700

    javaScript console

---
+++ partI/javaBasic_01.js
@@ -0,0 +1,16 @@
+// terminal 
+// ctrl + j => Open Terminal in Vs Code 
+// cd partI => node node javaBasic_01.js
+
+// Example 1 JavaScript console 
+console.log("Hello world")
+console.error("This is an Error")
+console.warn("")
+console.table({
+      name: "Sitthikorn",
+      age: 43,
+      occupation: ["employed"]
+})
+// Test the code in Devtool in Chrome Browser
+
+
